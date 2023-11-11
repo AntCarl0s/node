11d1498c341a38a3e0b4853e9feb4d54de58705c
AntCarl0s <143219379+AntCarl0s@users.noreply.github.com>
2023-11-10 23:08:19 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+// Requisita o módulo Express instalado para que possa ser utilizado na aplicação
+const express = require('express')
+
+// Cria uma instância da aplicação/Express
+const app = express()
+
+// Registra uma rota get(sinalizando leitura) apontando para a raiz '/'. Exemplo, exemplo.com/
+// req: Request - Em resumo, corresponde as entradas, tudo aquilo que é enviado para o servidor 
+// res: Response - Corresponde as saídas, tudo aquilo que é desejado enviar para o exterior
+app.get('/', (req, res) => {
+  // Envia uma string de resposta para a requisição realizada
+  res.send('Olá mundo!')
+})
+
+// Inicializa o servidor observando a porta 3000
+app.listen(3000, () => {
+  console.log('Server online')
+})
+
+app.get('/', (req, res) => {
+    res.send('Olá mundo.Ir para sobre')
+  })
